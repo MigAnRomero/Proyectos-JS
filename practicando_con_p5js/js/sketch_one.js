de908de164427a35147989cdb9c9e77d3037439f
MRomero <romerolaguado@gmail.com>
2022-11-11 11:24:40 -0500

    Aprendiendo a usar la función rect()

--- practicando_con_p5js/js/sketch_one.js
+++ practicando_con_p5js/js/sketch_one.js
@@ -6,8 +6,19 @@ function setup() {
 }
 
 function draw() {
-    line(0, 0, 400, 400);
-    line(0, 400, 400, 0);
-    line(0, 200, 400, 200);
-    line(200, 0, 200, 400);
+    /* Eje de coordenadas
+    line(x1, y1, x2, y2);
+    */
+    // line(0, 0, 400, 400);
+    // line(0, 400, 400, 0);
+    // line(0, 200, 400, 200);
+    // line(200, 0, 200, 400);
+
+    /* Rectangulo
+    rect(x1, y1, ancho, alto);
+    */
+    rect(0, 0, 200, 220);
+    rect(10, 10, 25, 15);
+    rect(10, 30, 25, 35);
+    rect(38, 10, 25, 55);
 }
