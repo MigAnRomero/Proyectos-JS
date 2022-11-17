a8948bfc707af76ec64580dff2724bdf04a1e6fb
MRomero <romerolaguado@gmail.com>
2022-11-17 10:39:05 -0500

    Recordando los comandos Confirm() y Prompt() de JS.

---
+++ practicando_sin_p5js/js/script_uno.js
@@ -0,0 +1,22 @@
+// Comandos:
+// Ejemplo de Confirm
+// var x = 1;
+// var y = 2;
+// var z = x + y;
+// var resp = confirm("¿Deseas sumar "+ x +" mas "+ y + "?");
+// if (resp == true) {
+// mensaje = "Resultado: " + z;
+// } else {
+// mensaje = "Terminado";
+// }
+
+// Ejemplo de Prompt
+// var x = 1;
+// var y = 2;
+// var z = x + y;
+// var resp = prompt("La suma de "+ x +" + "+ y + " es:");
+// if (resp == z) {
+// mensaje = "Correcto";
+// } else {
+// mensaje = "Incorrecto";
+// }
