a37634eaddad58f77c385bc482870cc729f55a1f
Miguel Romero <romerolaguado@gmail.com>
2022-11-21 21:28:56 -0500

    Pasando el código del script a un archivo js

---
+++ practicando_sin_p5js/js/codigo.js
@@ -0,0 +1,4 @@
+let ruta = window.location;
+console.log("Mensaje por leer... ");
+console.log(ruta);  // En consola aparece más cosas para saber como esta escrito por dentro la variable ruta
+document.write("Estás en: " + ruta);
