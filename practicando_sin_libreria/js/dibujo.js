88089742dda4a1fee13ba810facf4fa90e059ba6
MRomero <romerolaguado@gmail.com>
2022-11-29 11:16:37 -0500

    Jugando con el código.

--- practicando_sin_libreria/js/dibujo.js
+++ practicando_sin_libreria/js/dibujo.js
@@ -1,30 +1,29 @@
 window.onload = function() {
-    var d = document.getElementById("dibujito");
-    var lienzo = d.getContext("2d");
+    var canvas = document.getElementById("dibujito");
+    var lienzo = canvas.getContext("2d");
     var i;
     var j;
-    var xi,xy,xf,yf;
+    var xi,yi,xf,yf;
 
     function dibujarLinea(color,x_i,y_i,x_f,y_f){
         lienzo.beginPath();
-        lienzo.strokeStyle =color;
+        lienzo.strokeStyle = color;
         lienzo.moveTo(x_i,y_i);
         lienzo.lineTo(x_f,y_f);
         lienzo.stroke();
         lienzo.beginPath();
     }
     
-    for (i = 1; i <= 300; i=i+3) { 
+    for (i = 1; i <= 300; i = i + 3) { 
         j = i %2;
         if (j == 0) {
             xi = i;
             yi = 1;
-            xf = 300;
+            xf = 150;
             yf = i;
         } else {
-            
             xi = i;
-            yi = 300;
+            yi = 150;
             xf = 1;
             yf = i;
         }
