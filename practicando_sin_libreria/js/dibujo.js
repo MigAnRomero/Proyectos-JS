329ee18628f196aa68a8ebbdde7fdf07a80acc14
MRomero <romerolaguado@gmail.com>
2022-12-01 08:45:19 -0500

    Dibujando dos triángulos sin relleno de color rojo y amarillo

--- practicando_sin_libreria/js/dibujo.js
+++ practicando_sin_libreria/js/dibujo.js
@@ -14,4 +14,9 @@ window.onload = function() {
     }
 
     dibujarLinea("red", 0, 0, 150, 150);
+    dibujarLinea("yellow", 0, 4, 146, 150);
+    dibujarLinea("red", 150, 150, 200, 75);
+    dibujarLinea("yellow", 146, 150, 190, 75);
+    dibujarLinea("red", 200, 75, 0, 0); // Se cierra el triangulo rojo
+    dibujarLinea("yellow", 190, 75, 0, 4); // Se cierra el triangulo amarillo
 }
