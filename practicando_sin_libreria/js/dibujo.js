8cee1fd2b5b1f3149dd6e29c0a902ef7dd031c17
Miguel Romero <romerolaguado@gmail.com>
2022-12-03 20:39:33 -0500

    Creando un campo de texto para digitar un número qué pide la cantidad de líneas a dibujar.
    Se crea un evento cuando es oprimido el botón para mostrar el canvas dibujado por el número de líneas especificado.

--- practicando_sin_libreria/js/dibujo.js
+++ practicando_sin_libreria/js/dibujo.js
@@ -5,11 +5,8 @@ window.onload = function() {
     boton.addEventListener("click", dibujoPorClick);
     // Código para dibujar líneas
     let canvas = document.getElementById("dibujito");
+    let ancho = canvas.width; // Ancho del canvas
     let lienzo = canvas.getContext("2d");
-    let lineas = 30;  // Límite de líneas creadas
-    let ele = 0;
-    let xI, yI, xF, yF;
-    let colorLinea = "#AAF";
 
     // Función para dibujar una línea en el canvas 
     function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal)
@@ -24,7 +21,32 @@ window.onload = function() {
     // Función para cuando un btn es oprimido
     function dibujoPorClick()
     {
-        alert("Btn oprimido");
+        let lineas = parseInt(texto.value);;  // Límite de líneas creadas
+        let ele = 0;
+        let xI, yI, xF, yF;
+        let colorLinea = "#AAF";
+        let espacio = ancho / lineas;
+
+        for (ele = 0; ele < lineas; ele++)
+        {
+            yI = (espacio * ele);
+            xF = espacio * (ele + 1);
+            dibujarLinea(colorLinea, 0, yI, xF, 300);
+            console.log("Línea nro: " + ele);
+        }
+    
+        for (ele = 0; ele < lineas; ele++)
+        {
+            xI = (espacio * ele);
+            yF = espacio * (ele + 1);
+            dibujarLinea(colorLinea, xI, 0, 300, yF);
+            console.log("Línea dos nro: " + ele);
+        }
+
+        dibujarLinea("pink", 1, 1, 1, 299);  //Dibujar línea vertical en eje Y
+        dibujarLinea("pink", 1, 299, 299, 299);  //Dibujar línea vertical en eje X
+        dibujarLinea("pink", 1, 1, 299, 1);
+        dibujarLinea("pink", 299, 1, 299, 299);
     }
 
     // while (ele < lineas) 
@@ -35,25 +57,4 @@ window.onload = function() {
     //     console.log("Línea nro: " + ele);
     //     ele++;
     // }
-
-    for (ele = 0; ele < lineas; ele++)
-    {
-        yI = (10 * ele);
-        xF = 10 * (ele + 1);
-        dibujarLinea(colorLinea, 0, yI, xF, 300);
-        console.log("Línea nro: " + ele);
-    }
-
-    for (ele = 0; ele < lineas; ele++)
-    {
-        xI = (10 * ele);
-        yF = 10 * (ele + 1);
-        dibujarLinea(colorLinea, xI, 0, 300, yF);
-        console.log("Línea dos nro: " + ele);
-    }
-
-    dibujarLinea("pink", 1, 1, 1, 299);  //Dibujar línea vertical en eje Y
-    dibujarLinea("pink", 1, 299, 299, 299);  //Dibujar línea vertical en eje X
-    dibujarLinea("pink", 1, 1, 299, 1);
-    dibujarLinea("pink", 299, 1, 299, 299);
 }
